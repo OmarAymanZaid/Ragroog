64cfba8ff0190a5e8ada680c394d1601b3c13eed
OmarAyman <omaraymanzaid1122@gmail.com>
2026-07-12 13:52:57 +0300

    Add file processing endpoint to the data.py router file

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,8 +5,9 @@ from fastapi.responses import JSONResponse
 from loguru import logger
 
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
+from controllers import DataController, ProjectController, ProcessController
+from .schemes.data import ProcessRequest
 
 
 data_router = APIRouter(
@@ -59,3 +60,31 @@ async def upload_data(project_id: str, file: UploadFile,
                 "file_id": file_id
             }
         )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+
+    return file_chunks
