98547423f113388b9cafcb57edaa8942ab40a54c
OmarAyman <omaraymanzaid1122@gmail.com>
2026-07-17 17:54:49 +0300

    Uncomment import of NLPController in controllers initialization

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,4 +1,5 @@
+from .BaseController import BaseController
 from .DataController import DataController
 from .ProjectController import ProjectController
 from .ProcessController import ProcessController
-# from .NLPController import NLPController
+from .NLPController import NLPController
