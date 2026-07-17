48ed4b1ac3b09a4c881215cea7f1c27a5d642c97
OmarAyman <omaraymanzaid1122@gmail.com>
2026-07-17 15:05:44 +0300

    Add CoHereProvider instantiation in LLMProviderFactory

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -16,6 +16,12 @@ class LLMProviderFactory:
                 default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
             )
 
-	
+        if provider == LLMEnums.COHERE.value:
+            return CoHereProvider(
+                api_key = self.config.COHERE_API_KEY,
+                default_input_max_characters=self.config.INPUT_DAFAULT_MAX_CHARACTERS,
+                default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+            )
 
         return None
